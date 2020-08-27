e2b94c4c315eb9707388d1fc03ac6511736d2ff5
Felipe Dantas <feliperbdantas@outlook.com>
2020-08-26 21:40:19 -0300

    setup

---
+++ src/main/java/br/com/felipedantas/springbootadmin/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipedantas.springbootadmin.springbootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
